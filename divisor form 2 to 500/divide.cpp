a68f7394fa98a829a97d471b5831937f5f5d47dc
Saifullah Mansoori <134608774+SaifullahMansoori@users.noreply.github.com>
2024-03-27 11:50:20 -0700

    Add files via upload

---
+++ divisor form 2 to 500/divide.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int i = 2; i <= 500; ++i)
+    {
+        cout << i << " : ";
+        for (int j = 1; j <= i / 2; ++j)
+        {
+            if (i % j == 0)
+                cout << j << '\t';
+        }
+        cout << i ;
+        cout << endl;
+    }
+
+
+return 0;
+}
