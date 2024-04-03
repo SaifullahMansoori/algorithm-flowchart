82ef11c8ec5cd51ae7476108aa403ea6472e6c3e
Saifullah Mansoori <134608774+SaifullahMansoori@users.noreply.github.com>
2024-04-03 13:27:29 -0700

    Add files via upload

---
+++ printing odd num till n/odd.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num = 1;
+    int target = 8;
+    while (num < target)
+    {
+        cout << num << endl;
+        num += 2;
+    }
+
+return 0;
+}
